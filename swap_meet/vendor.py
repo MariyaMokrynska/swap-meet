1da87c77834090e49fd737644633c52b9c06c812
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-03-31 20:00:23 -0700

    Implemented class Vendor and methods, passed 3/5 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,13 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory is None else inventory
+
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+    
+    def remove(self, item_to_remove):
+        self.inventory.remove(item_to_remove)
+        if not item_to_remove in self.inventory:
+            return False
+        return item_to_remove
