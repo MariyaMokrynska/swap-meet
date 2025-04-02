fa937d6e381253204737fa9006e5c13f28452055
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-04-01 20:43:48 -0700

    Implements swap_items method, adds assertion section to the test, all tests passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -26,3 +26,19 @@ class Vendor:
             if item.id == id:
                 return item
         return None
+    
+    ### WAVE 3
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        # Swap fails if item is missing in self or other_vendor inventory
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        
+        # Successful swap: remove and add the items from/to self and other_vendor inventories
+        self.inventory.remove(my_item) 
+        other_vendor.inventory.append(my_item)
+
+        other_vendor.inventory.remove(their_item) 
+        self.inventory.append(their_item)
+        
+        return True 
--- tests/unit_tests/test_wave_03.py
+++ tests/unit_tests/test_wave_03.py
@@ -131,7 +131,11 @@ def test_swap_items_from_their_empty_returns_false():
 
     result = fatimah.swap_items(jolie, item_b, nobodys_item)
 
-    raise Exception("Complete this test according to comments below.")
+    # raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
+
+    assert len(fatimah.inventory) == 3
+    assert len(jolie.inventory) == 0
+    assert not result
