48bf9d5c4b89f6b2efcf451dd00908a51c03ba60
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-04-01 17:52:33 -0700

    implemented wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,23 @@
+import uuid
+
+
 class Item:
-    pass
+    def __init__(self, id=None):  # default id is None
+        """
+        Initializes an Item with a unique ID.
+        If no ID is provided, a random unique ID is generated using uuid.
+
+        Written By Mariya Mokrynska
+        """
+        if id == None:
+            self.id = uuid.uuid4().int  # unique id
+        else:
+            self.id = id
+
+    def get_category(self):
+        """
+        Returns the name of the class as a string.
+
+        Written By Mariya Mokrynska
+        """
+        return self.__class__.__name__
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,6 @@
+from .item import Item
+
+
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
@@ -5,9 +8,21 @@ class Vendor:
     def add(self, new_item):
         self.inventory.append(new_item)
         return new_item
-    
+
     def remove(self, item_to_remove):
         if item_to_remove in self.inventory:
             self.inventory.remove(item_to_remove)
             return item_to_remove
         return False
+
+    def get_by_id(self, id):
+        """
+        Searches for an item in the inventory by its ID.
+        Returns the item if found, otherwise returns None.
+
+        Written By Mariya Mokrynska
+        """
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
