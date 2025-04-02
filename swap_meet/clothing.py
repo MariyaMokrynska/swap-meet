5064764c1323615ad64a7cfd1fb70d36e86031c1
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-04-02 14:26:53 -0700

    implemented module Clothing

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,16 @@
-class Clothing: # test jane
-    pass
+from uuid import uuid4 
+
+class Clothing: 
+    def __init__(self, id=None, fabric="Unknown"):
+        self.id = uuid4().int if id is None else id
+        self.fabric = fabric if fabric != "Unknown" else "Unknown"
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}."\
+        " It is made from {self.fabric} fabric."
+    
+item_a = Clothing(123435, "Wool")
+print(str(item_a))
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -27,3 +27,4 @@ class Item:
     # Returns a string representation of the Item instance
     def __str__(self):
         return f"An object of type Item with id {self.id}."
+    
