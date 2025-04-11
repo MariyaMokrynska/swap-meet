576b19b6a9df20a5300b49ce83c3ad113ddf3a15
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-04-11 16:18:27 -0700

    Refactored constructors using inverted logic in decor.py and electronics.py

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,14 +1,16 @@
-### WAVE 5
+# WAVE 5
 from .item import Item
 
-class Decor(Item): # Updated class to inherit from Item
+
+class Decor(Item):  # Updated class to inherit from Item
     def __init__(self, id=None, width=0, length=0, condition=0):
-        super().__init__(id, condition)  # pass id and condition to Item, move 
-                                        # the line above child's custom attribute
-        self.width = 0 if width == 0 else width
-        self.length = 0 if length == 0 else length
-    
+        super().__init__(id, condition)  # pass id and condition to Item, move
+        # the line above child's custom attribute
+        # self.width = 0 if width == 0 else width
+        # self.length = 0 if length == 0 else length
+        self.width = width if width else 0
+        self.length = length if length else 0
+
     def __str__(self):
         return f"{super().__str__()} It takes up a {self.width} by "\
             f"{self.length} sized space."
-
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,10 +1,12 @@
 from .item import Item
 
-class Electronics(Item): # Updated class to inherit from Item
+
+class Electronics(Item):  # Updated class to inherit from Item
     def __init__(self, id=None, type="Unknown", condition=0):
-        super().__init__(id, condition)  # pass id and condition to Item, move 
-                                        # the line above child's custom attribute
-        self.type = "Unknown" if type == "Unknown" else type
-    
+        super().__init__(id, condition)  # pass id and condition to Item, move
+        # the line above child's custom attribute
+        # self.type = "Unknown" if type == "Unknown" else type
+        self.type = type if type != "Unknown" else "Unknown"
+
     def __str__(self):
         return f"{super().__str__()} This is a {self.type} device."
